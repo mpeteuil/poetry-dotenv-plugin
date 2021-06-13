5f21724d95d03aef8f418cd18888e387697c4cea
Michael Peteuil <michael.peteuil@gmail.com>
2021-06-13 17:30:19 -0400

    Run system tests in a shell script

---
+++ tests/test_system.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+function create_dotenv_file() {
+  # Setup .env file so the plugin can do its job
+  echo "export MY_ENV_VAR='foo'" > .env
+}
+
+function delete_dotenv_file() {
+  # Tear down .env file since we no longer need it
+  rm .env
+}
+
+function test_end_to_end_system_with_default_dotenv_file() {
+  local expected
+  expected='foo'
+
+  create_dotenv_file
+
+  local output
+  output=$(poetry run python -c "import os; print(os.environ['MY_ENV_VAR'])")
+
+  delete_dotenv_file
+
+  if [ "$expected" = "$output" ]; then
+    printf "test_end_to_end_system_with_default_dotenv_file: PASSED\n"
+  else
+    printf "Expected '$expected', but got '%s'.\n" "$output"
+    exit 1
+  fi
+}
+
+function test_end_to_end_system_with_dotenv_location_override() {
+  local expected
+  expected='bar'
+  local new_dotenv_path
+  new_dotenv_path="$PWD/nested/path"
+
+  create_dotenv_file
+
+  # Override the file that was just created
+  mkdir -p "$new_dotenv_path" && echo "export MY_ENV_VAR='bar'" > "$new_dotenv_path/.env"
+
+  local output
+  output=$(export POETRY_DOTENV_LOCATION="$new_dotenv_path/.env" && poetry run python -c "import os; print(os.environ['MY_ENV_VAR'])")
+
+  delete_dotenv_file
+
+  if [ "$expected" = "$output" ]; then
+    printf "test_end_to_end_system_with_dotenv_location_override: PASSED\n"
+  else
+    printf "Expected '$expected', but got '%s'.\n" "$output"
+    exit 1
+  fi
+}
+
+function test_end_to_end_system_without_loading_dotenv_file() {
+  local expected
+  expected='Nonexistent Variable'
+
+  create_dotenv_file
+
+  local output
+  output=$(export POETRY_DONT_LOAD_ENV=true && poetry run python -c "import os; print(os.environ.get('MY_ENV_VAR', '$expected'))")
+
+  delete_dotenv_file
+
+  if [ "$expected" = "$output" ]; then
+    printf "test_end_to_end_system_without_loading_dotenv_file: PASSED\n"
+  else
+    printf "Expected '$expected', but got '%s'.\n" "$output"
+    exit 1
+  fi
+}
+
+test_end_to_end_system_with_default_dotenv_file
+test_end_to_end_system_with_dotenv_location_override
+test_end_to_end_system_without_loading_dotenv_file
